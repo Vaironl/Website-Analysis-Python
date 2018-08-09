ff667c1394d6e5810371ca1e2acd4cf98513dd36
nmendoza <vmend001@odu.edu>
2018-08-09 18:49:19 -0400

    Makefile helps to create requirements.txt

---
+++ Analysis/usercli.py
@@ -0,0 +1,8 @@
+# The program should ask the user to enter 2+ paramater
+# one being a path to the local copy of the site
+# the other being one or more URLs
+
+class UserCLI:
+    def method(*args):
+        for argument in args:
+            print "test"
