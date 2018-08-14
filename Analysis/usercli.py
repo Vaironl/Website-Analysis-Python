ed3b6c070631bb60bd73766e5748cecae757c69a
nmendoza <vmend001.odu.edu>
2018-08-14 18:49:26 -0400

    Added some helper methods, needs more restructuring of the testing class

--- Analysis/usercli.py
+++ Analysis/usercli.py
@@ -2,10 +2,22 @@
 # one being a path to the local copy of the site
 # the other being one or more URLs
 
-class UserCLI:
-    def analyzewebsite(*args):
-        if len(args) < 2:
-            raise TypeError("There must be at least 2 arguments")
-        for argument in args:
-            pass
+import sys
 
+arguments = None
+
+if __name__ != "__main__":
+    def setupArguments(args):
+        global arguments
+        arguments = args
+
+elif __name__ == "__main__":
+    global arguments
+    arguments = sys.argv
+
+
+def startAnalysis():
+    if len(arguments) < 2:
+        raise TypeError("There must be at least 2 arguments")
+    elif len(arguments) > 1001:
+        raise TypeError("There must be no more than 1,001 arguments")
--- test/userclitest.py
+++ test/userclitest.py
@@ -1,10 +1,17 @@
 import unittest
 
-from Analysis.usercli import UserCLI
+import Analysis
+from Analysis.usercli import setupArguments, startAnalysis
 
+emptylist = []
 
-class UserCLITests(unittest.TestCase):
-    def test_analyzewebsite(self):
-        with self.assertRaises(TypeError):
-            UserCLI.analyzewebsite()
+for i in range (0,2000):
+    emptylist.append(i)
 
+class TestUserCLI(unittest.TestCase):
+
+    def test_setupArguments(self):
+        setupArguments(emptylist)
+
+    def test_startAnalysis(self):
+        startAnalysis()
