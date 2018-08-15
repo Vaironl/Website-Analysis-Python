1c0df5bbe68d33fca2be449387512bd5b7e3500f
nmendoza <vmend001@odu.edu>
2018-08-15 13:19:20 -0400

    Cleaner more pythonic code and TDD is being followed more strictly

--- Analysis/usercli.py
+++ Analysis/usercli.py
@@ -1,23 +1,11 @@
-# The program should ask the user to enter 2+ paramater
-# one being a path to the local copy of the site
-# the other being one or more URLs
+class UserCLI:
+    arguments = None
+    if __name__ != "__main__":
+        def setup_arguments(self,arguments):
+            pass
 
-import sys
+    elif __name__ == "__main__":
+        pass
 
-arguments = None
-
-if __name__ != "__main__":
-    def setupArguments(args):
-        global arguments
-        arguments = args
-
-elif __name__ == "__main__":
-    global arguments
-    arguments = sys.argv
-
-
-def startAnalysis():
-    if len(arguments) < 2:
-        raise TypeError("There must be at least 2 arguments")
-    elif len(arguments) > 1001:
-        raise TypeError("There must be no more than 1,001 arguments")
+    def start_analysis(self):
+        pass
--- test/userclitest.py
+++ test/userclitest.py
@@ -1,17 +1,37 @@
 import unittest
 
-import Analysis
-from Analysis.usercli import setupArguments, startAnalysis
+from Analysis.usercli import UserCLI
 
-emptylist = []
-
-for i in range (0,2000):
-    emptylist.append(i)
 
 class TestUserCLI(unittest.TestCase):
-
     def test_setupArguments(self):
-        setupArguments(emptylist)
+
+        empty_list = []
+        one_arg_list = []
+        two_arg_list = []
+        valid_range_list = []
+        over_sized_list = []
+
+        for i in range(0, 20):
+            valid_range_list.append(i)
+
+        for i in range(0, 2000):
+            over_sized_list.append(i)
+
+        self.assertEqual(UserCLI.setupArguments(two_arg_list), "Number of arguments are valid!")
+        self.assertEqual(UserCLI.setupArguments(valid_range_list), "Number of arguments are valid!")
+
+        with self.assertRaises(TypeError):
+            UserCLI.setupArguments(empty_list)
+
+        with self.assertRaises(TypeError):
+            UserCLI.setupArguments(one_arg_list)
+
+        with self.assertRaises(TypeError):
+            UserCLI.setupArguments(over_sized_list)
 
     def test_startAnalysis(self):
-        startAnalysis()
+        UserCLI.startAnalysis()
+        #Validate Path and validate links in startAnalysis
+        #If path is not valid throw an error
+        #If url(s) are not valid throw an error
