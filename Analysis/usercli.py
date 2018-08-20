6949ea998882530ee85f82e9862eac6848423670
nmendoza <vmend001@odu.edu>
2018-08-20 11:29:19 -0400

    Implementation completed

--- Analysis/usercli.py
+++ Analysis/usercli.py
@@ -8,15 +8,22 @@ class UserCLI:
     def setup_test(self, in_argv):
         """Helps to test class"""
         global argv
-        # argv = in_argv
+        argv = in_argv
 
         pass
 
     def start_analysis(self):
+        if len(argv) == 3:
+            urls = argv[2]
+            if urls == []:
+                return False
+            if len(urls) < 0 or len(urls) > 1000:
+                return False
+            return True
         return False
 
     def print_results(self):
-        #
+        """Results to be printed"""
         pass
 
     def get_arv(self):
