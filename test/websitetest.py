7248e9fdcf86490fced4824a06699ba88bb1595e
nmendoza <vmend001@odu.edu>
2018-08-21 20:32:44 -0400

    Added tests vor urls and local path

--- test/websitetest.py
+++ test/websitetest.py
@@ -1,15 +1,36 @@
 import unittest
 
-from Analysis.website import map_urls
+from Analysis.website import map_urls, validate_local_path, validate_urls
 
 
 class TestWebsite(unittest.TestCase):
 
-    def test_validate_local_path(self):
-        pass
+    def test_validate_local_path_valid(self):
+        test_website_path = "test/testwebsite"
+        self.assertTrue(validate_local_path(test_website_path))
+
+    def test_validate_local_path_invalid(self):
+        invalid_website_path = "test/somebadlocalpath"
+        self.assertFalse(validate_local_path(invalid_website_path))
 
     def test_validate_urls(self):
-        pass
+        # use django to validate
+        all_urls = [
+            "https://www.cs.odu.edu/~tkennedy/cs350/sum18/Directory/outline/index.html",
+            "https://www.cs.odu.edu/~tkennedy/doesnotexist.html",
+            "https://www.cs.odu.edu/~tkennedy/cs350/sum18/Public/syllabus/index.html",
+            "https://www.arandomnonexistentwebsite.com",
+            "https://www.cs.odu.edu/~tkennedy/cs350/sum29183094/Public/syllabus/index.html",
+            "google.com"
+        ]
+
+        valid_urls = [
+            "https://www.cs.odu.edu/~tkennedy/cs350/sum18/Directory/outline/index.html",
+            "https://www.cs.odu.edu/~tkennedy/cs350/sum18/Public/syllabus/index.html",
+            "https://www.cs.odu.edu/~tkennedy/cs350/sum29183094/Public/syllabus/index.html"
+        ]
+
+        self.assertEqual(validate_urls(all_urls), valid_urls)
 
     def test_map_urls(self):
         site_root = "www.cs.odu.edu/~tkennedy/cs350/sum18/"
